d0101a9a1ee7c8eeb2d36689fcb49e7722114690
shiboshreeroy <shiboshreeroy169@gmail.com>
2025-03-22 15:54:27 +0600

    update project

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create!(
+  email: 'manuroy169@gmail.com',
+  password: 'xpose009@',
+  password_confirmation: 'Xpose009@',
+  admin: true,
+)
+User.create!(
+  email:"orgatro.it.official@gmail.com',
+  password: 'orgatro009@',
+  password_confirmation: 'orgatro009@',
+  admin: true,
+)
